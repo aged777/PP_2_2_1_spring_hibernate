2ed4e72069cb56ccbcb80a83c4caaf3a8ae4619e
Aleksandr Volkov <222volkov@gmail.com>
2023-01-22 18:04:09 +0300

    class Car modified according to the conditions

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -10,20 +10,16 @@ public class Car {
     private Long id;
     @Column(name = "model")
     private String model;
-    @Column(name = "age")
-    private int age;
-    @Column(name = "color")
-    private String color;
-
+    @Column(name = "series")
+    private int series;
 
     public Car() {
 
     }
 
-    public Car(String model, int age, String color) {
+    public Car(String model, int series) {
         this.model = model;
-        this.age = age;
-        this.color = color;
+        this.series = series;
     }
 
 
@@ -43,19 +39,11 @@ public class Car {
         return model;
     }
 
-    public void setModel(int age) {
-        this.age = age;
+    public void setSeries(int age) {
+        this.series = age;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getColor() {
-        return color;
+    public int getSeries() {
+        return series;
     }
 }
