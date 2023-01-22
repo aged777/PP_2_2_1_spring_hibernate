f118cba8f085990c1f25a7d34647cda4150cc283
Aleksandr Volkov <222volkov@gmail.com>
2023-01-22 12:57:28 +0300

    task_2_2_1 POJO class Car created

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,61 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "color")
+    private String color;
+
+
+    public Car() {
+
+    }
+
+    public Car(String model, int age, String color) {
+        this.model = model;
+        this.age = age;
+        this.color = color;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
